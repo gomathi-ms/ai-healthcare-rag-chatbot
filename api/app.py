2af2aba840df76563b07dbd394fa399d1ab4b7d7
gomathi-ms <142835902+gomathi-ms@users.noreply.github.com>
2026-03-16 15:46:54 -0500

    Initialized FastAPI app with chat endpoint

---
+++ api/app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from src.rag_pipeline import create_rag_chain
+
+app = FastAPI()
+
+@app.get("/chat")
+
+def chat(query: str):
+
+    answer = rag_chain.invoke(query)
+
+    return {"response": answer}
