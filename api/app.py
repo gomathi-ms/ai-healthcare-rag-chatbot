d2e2d3d2315433cb8c6a04e618c39ec5bb7f51e2
gomathi-ms <142835902+gomathi-ms@users.noreply.github.com>
2026-03-16 16:21:01 -0500

    Fixing the endpoint

--- api/app.py
+++ api/app.py
@@ -4,9 +4,6 @@ from src.rag_pipeline import create_rag_chain
 app = FastAPI()
 
 @app.get("/chat")
-
 def chat(query: str):
-
     answer = rag_chain.invoke(query)
-
     return {"response": answer}
